cf76cfeafe949663506b5f7ddf6199f85499299a
chengetanai <mukanhairic@gmail.com>
2025-02-15 15:20:46 +0200

    feat: add navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
-import { useState } from "react";
+import { NavBar } from "./components/NavBar";
 
 function App() {
-  const [amount, setAmount] = useState("10");
-
   return (
     <>
-      <div>Chasing me {amount} thou' that's inshallah</div>
-      <button
-        className="bg-black text-white rounded-xl p-1 m-3 cursor-pointer"
-        onClick={() => setAmount(`${amount + 0}`)}
-      >
-        Racks
-      </button>
+      <NavBar></NavBar>
     </>
   );
 }
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,27 @@
+export function NavBar() {
+  return (
+    <div className="flex justify-between px-6 py-2 ">
+      <div className="flex gap-x-10">
+        <a className="font-bold">Cancer Prediction</a>
+        <a href="" className="font-thin">
+          Machine Learning in Health
+        </a>
+        <a href="" className="font-thin">
+          Better Living
+        </a>
+        <a href="" className="font-thin">
+          Tour Guide
+        </a>
+      </div>
+      <div>
+        <div className="flex gap-x-2">
+          <input
+            type="text"
+            placeholder="Search documentation..."
+            className="border-2 rounded-lg placeholder:text-slate-400 focus:outline-none py-1 px-3 text-sm"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
