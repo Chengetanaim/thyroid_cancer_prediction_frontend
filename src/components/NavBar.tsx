2ac77dc89667caaff2c9f80699d3dbec79f51d85
chengetanai <mukanhairic@gmail.com>
2025-02-15 18:38:21 +0200

    refactor: make minor changes to navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,7 +2,7 @@ export function NavBar() {
   return (
     <>
       <div className="bg-gray-50">
-        <div className="flex justify-between px-6 py-2 ">
+        <div className="flex justify-between px-6 py-4 ">
           <div className="flex gap-x-10">
             <a className="font-bold">Thyroid Cancer Prediction</a>
             <a href="" className="font-thin">
@@ -12,20 +12,20 @@ export function NavBar() {
               Better Living
             </a>
             <a href="" className="font-thin">
-              Tour Guide
+              Medical Professionals
             </a>
           </div>
           <div>
             <div className="flex gap-x-2">
               <input
                 type="text"
-                placeholder="Search documentation..."
+                placeholder="Search symptoms..."
                 className="border-2 rounded-lg placeholder:text-slate-400 focus:outline-none py-1 px-3 text-sm"
               />
             </div>
           </div>
         </div>
-        <div className="mt-6 border-b border-gray-200"></div>
+        <div className=" border-b border-gray-200"></div>
       </div>
     </>
   );
